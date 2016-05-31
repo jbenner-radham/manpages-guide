6b51769f9e2eb4c4ab50ec5177e56494ccc43d8e
James Benner <james.benner@gmail.com>
2016-05-31 19:46:47 -0400

    Added some stuff and things...

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const $           = require('gulp-load-plugins')();
+const browserSync = require('browser-sync').create();
+const dateTime    = require('@radioactivehamster/date-time');
+const fs          = require('fs');
+const gulp        = require('gulp');
+const pkg         = require('./package.json');
+const yaml        = require('js-yaml');
+
+gulp.task('js', () => {
+    return gulp.src('src/js/**/*.js')
+        .pipe($.eslint())
+        .pipe($.eslint.format())
+        .pipe($.eslint.failAfterError())
+        .pipe($.plumber())
+        .pipe($.sourcemaps.init())
+        .pipe($.babel())
+        .pipe($.sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('serve', ['js'], () => {
+    browserSync.init({
+        notify: true,
+        port:   9000,
+        server: {
+            baseDir: ['.tmp', 'app']
+        }
+    });
+
+    gulp.watch([
+        'app/*.html',
+        '.tmp/**/*.*'
+    ]).on('change', browserSync.reload);
+
+    gulp.watch('.tmp/js/**/*.js', ['scripts']);
+});
+
+gulp.task('handlebars', () => {
+    let author = pkg.author.replace(/ <.+>/i, '');
+    let tidyrc = yaml.load(fs.readFileSync('./.tidyrc').toString());
+
+    return gulp.src('./src/templates/**/*.hbs')
+        .pipe($.stachio({author: author, timestamp: dateTime()}))
+        .pipe($.htmlhint())
+        .pipe($.htmlhint.reporter())
+        .pipe($.htmltidy(tidyrc))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['serve']);
